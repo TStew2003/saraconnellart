3bf4a0183c66acbe2317e97272ec4218b2154cfc
connelljack <connellj2@wit.edu>
2023-09-12 21:19:32 -0400

    fixed home page font and Title

--- js/homeSlides.js
+++ js/homeSlides.js
@@ -2,13 +2,13 @@ var i = 0;
 var images = [];
 var time = 3000;
 
-images[0] = '../images/IMG_0009.jpg';
-images[1] = '../images/IMG_0027.JPG';
+images[0] = '../images/IMG_6661.JPG';
+images[1] = '../images/IMG_2107.jpeg';
 images[2] = '../images/IMG_2117.jpeg';
-images[3] = '../images/IMG_2116.jpeg';
-images[4] = '../images/IMG_2114.jpeg';
-images[5] = '../images/IMG_2113.jpeg';
-images[6] = '../images/IMG_2112.jpeg';
+images[3] = '../images/IMG_7769.JPG';
+images[4] = '../images/IMG_0557.jpeg';
+images[5] = '../images/IMG_9823.jpg.jpeg';
+
 
 
 
